9bebc062069d68f6feae708cd7ca023d08bcc93e
Kaippy <kaippy.canijo@gmail.com>
2025-07-22 19:27:17 -0300

    Adicionar arquivos de projeto.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            // Obter um número alearório de 1 a 4
+            int value = random.Next(1, 5);
+            // Se for 1, retornará a string Spades
+            if (value == 1) return "Spades";
+            // Se for 2, retornará a string Hearts
+            if (value == 2) return "Hearts";
+            // Se for 3, retornará a string Clubs
+            if (value == 3) return "Clubs";
+            // Se ainda não retornamos, retornará a string Diamonds
+            return "Diamonds";
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Entre com o número de cartas para pegar ou digite qualquer outra tecla para sair: ");
+                string line = Console.ReadLine();
+            
+                if (int.TryParse(line, out int numbersOfCards))
+                {
+                    string[] cards = CardPicker.PickSomeCards(numbersOfCards);
+                    foreach (string card in cards)
+                    {
+                        Console.WriteLine(card);
+                    }
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
